145094dcc1fc324827d1ebc3332270bd01857e5a
Jasmine <jasmine.harit@knoldus.com>
2022-03-26 13:29:40 +0530

    Final_Capstone

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/chat-app",
+  mongoURI: "mongodb+srv://chat-app:jasmine123@cluster0.qw6uc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
